c7528867d6ae3fac3e923a3d108dc453ab317acb
Syafiqq <syafiq.rezpector@gmail.com>
2019-09-11 17:05:24 +0700

    add from scheduler

--- app/src/androidTest/java/com/github/syafiqq/rxandroidtest001/create/FromTest.kt
+++ app/src/androidTest/java/com/github/syafiqq/rxandroidtest001/create/FromTest.kt
@@ -6,12 +6,16 @@ import io.reactivex.observers.TestObserver
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
 class FromTest {
     @Test
     fun it_successful_create_with_future() {
-        val s = CompletableFuture.completedFuture(1)
+        val s = CompletableFuture.supplyAsync {
+            1
+        }
         val o = Observable.fromFuture(s)
 
         val subscriber = TestObserver<Int>()
@@ -20,5 +24,37 @@ class FromTest {
         subscriber.assertNoErrors()
         subscriber.assertComplete()
         subscriber.assertValue(1)
+    }
+
+    @Test
+    fun it_successful_create_with_future_with_time_unit() {
+        val s = CompletableFuture.supplyAsync {
+            Thread.sleep(100)
+            1
+        }
+        val o = Observable.fromFuture(s, 150L, TimeUnit.MILLISECONDS)
+
+        val subscriber = TestObserver<Int>()
+        o.subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.assertComplete()
+        subscriber.assertValue(1)
+    }
+
+    @Test
+    fun it_successful_create_with_future_with_surpass_time_unit() {
+        val s = CompletableFuture.supplyAsync {
+            Thread.sleep(100)
+            1
+        }
+        val o = Observable.fromFuture(s, 50L, TimeUnit.MILLISECONDS)
+
+        val subscriber = TestObserver<Int>()
+        o.subscribe(subscriber)
+
+        subscriber.assertError(TimeoutException::class.java)
+        subscriber.assertNotComplete()
+        subscriber.assertNoValues()
     }
 }

719049c44e699278434085b463fb4847089faf1c
Syafiqq <syafiq.rezpector@gmail.com>
2019-09-11 11:18:15 +0700

    add it_successful_access_next_and_complete_on_array

--- app/src/test/java/com/github/syafiqq/rxandroidtest001/create/CreateTest.kt
+++ app/src/test/java/com/github/syafiqq/rxandroidtest001/create/CreateTest.kt
@@ -55,4 +55,22 @@ class CreateTest {
         subscriber1.assertComplete()
         subscriber1.assertValue(1)
     }
+
+    @Test
+    fun it_successful_access_next_and_complete_on_array() {
+        val o = Observable.create<Int> {
+            for (i in 1..10) {
+                it.onNext(i)
+            }
+
+            it.onComplete()
+        }
+
+        val subscriber = TestObserver<Int>()
+        o.subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.assertComplete()
+        subscriber.assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    }
 }

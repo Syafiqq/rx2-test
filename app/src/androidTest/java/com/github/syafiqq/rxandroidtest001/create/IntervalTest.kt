0649eb79161e48264e2636b7aee2e9bfacaa8306
Syafiqq <syafiq.rezpector@gmail.com>
2019-09-12 09:40:18 +0700

    add more internal test

--- app/src/androidTest/java/com/github/syafiqq/rxandroidtest001/create/IntervalTest.kt
+++ app/src/androidTest/java/com/github/syafiqq/rxandroidtest001/create/IntervalTest.kt
@@ -3,6 +3,7 @@ package com.github.syafiqq.rxandroidtest001.create
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.TimeUnit
@@ -11,7 +12,36 @@ import java.util.concurrent.TimeUnit
 class IntervalTest {
     @Test
     fun it_successful_implement_interval() {
-        val o = Observable.interval(50, TimeUnit.MILLISECONDS).take(2)
+        val o = Observable.interval(50, TimeUnit.MILLISECONDS)
+
+        val subscriber = TestObserver<Long>()
+        o.subscribe(subscriber)
+
+        Thread.sleep(120)
+
+        subscriber.assertNoErrors()
+        subscriber.assertNotComplete()
+    }
+
+    @Test
+    fun it_successful_implement_interval_with_two_step() {
+        val o = Observable.interval(50, TimeUnit.MILLISECONDS)
+            .take(2)
+
+        val subscriber = TestObserver<Long>()
+        o.subscribe(subscriber)
+
+        Thread.sleep(120)
+
+        subscriber.assertNoErrors()
+        subscriber.assertComplete()
+        subscriber.assertValues(0, 1)
+    }
+
+    @Test
+    fun it_successful_implement_interval_with_two_step_and_immediate_scheduler() {
+        val o = Observable.interval(50, TimeUnit.MILLISECONDS, Schedulers.computation())
+            .take(2)
 
         val subscriber = TestObserver<Long>()
         o.subscribe(subscriber)
